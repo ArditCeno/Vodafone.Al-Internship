b8f5e0d0d8331dece69bfda6e6d35aa63d190aea
ArditCeno <arditceno1@gmail.com>
2026-05-25 17:13:52 +0200

    fix: hiq RestTemplate bean dup nga Tobi2Application (ekziston ne Tobi2DataConfig)

--- src/main/java/com/vodafone/tobi2/Tobi2Application.java
+++ src/main/java/com/vodafone/tobi2/Tobi2Application.java
@@ -2,18 +2,11 @@ package com.vodafone.tobi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Tobi2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Tobi2Application.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }

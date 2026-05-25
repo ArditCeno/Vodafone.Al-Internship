d112e117e537d508851e2d01d402fc782f51ab14
ArditCeno <arditceno1@gmail.com>
2026-05-25 16:37:49 +0200

    fix: shto RestTemplate bean, bashkoj build+test ne CI

--- src/main/java/com/vodafone/tobi2/Tobi2Application.java
+++ src/main/java/com/vodafone/tobi2/Tobi2Application.java
@@ -2,12 +2,18 @@ package com.vodafone.tobi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Tobi2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Tobi2Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
